4bddbd05c4a8fac5d01bb32af966fe61c79fdece
Gabe Hoban <ghoban14@gmail.com>
2020-02-21 19:23:26 -0500

    M  .zsh/alias.zsh

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -5,7 +5,8 @@ alias ls="ls -AGe"
 alias cat="bat "
 alias study='open -na "Brave Browser Nightly" --args --incognito "https://youtube.com/watch?v=MCkTebktHVc"'
 alias dots='cd "$HOME"/.dotfiles &&\
-            git yolo &&\
+            git add -A &&\
+            git commit -m "$(git status --porcelain)" &&\
             git push origin master &&\
             cd -;'
 alias pip="pip3"
